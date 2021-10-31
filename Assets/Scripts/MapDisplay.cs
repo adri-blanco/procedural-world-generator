1beb011e18719992de6ca14130cadf6e87205d04
Adri Blanco <adrian-b.blanco@shell.com>
2021-10-31 22:09:52 +0100

    We should update also the mesh collider

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -7,6 +7,7 @@ public class MapDisplay : MonoBehaviour
   public Renderer map;
   public MeshFilter meshFilter;
   public MeshRenderer meshRenderer;
+  public MeshCollider meshCollider;
 
   public void Draw(Texture2D texture)
   {
@@ -16,7 +17,9 @@ public class MapDisplay : MonoBehaviour
 
   public void DrawMesh(MeshData meshData, Texture2D texture)
   {
-    meshFilter.sharedMesh = meshData.createMesh();
+    Mesh mesh = meshData.createMesh();
+    meshFilter.sharedMesh = mesh;
+    meshCollider.sharedMesh = mesh;
     map.transform.localScale = new Vector3(texture.width, texture.height, 1);
     meshRenderer.sharedMaterial.mainTexture = texture;
   }
